74ca2bc28cf86d8b84d372e76f29fea05f51b098
bartph <50647914+bartph@users.noreply.github.com>
2022-10-06 17:31:50 +0800

    Update dailypass.js

--- YLGY/dailypass.js
+++ YLGY/dailypass.js
@@ -1,8 +1,11 @@
+//let url = $request.url;
+$request.url = 'https://github.com/bartph/shadowrocketrules/edit/main/YLGY/dailypass.js';
 let status = $response.status;
+console.log($request.url);
+console.log($request.url.path);
 if(status == 200){
   let body = $response.body;
   let obj = JSON.parse(body);
   obj.data.map_seed = [0,0,0,0];
-  obj.data.map_seed_2 = "0000000000";
   $done({body: JSON.stringify(obj)});
 }  else $done({});
