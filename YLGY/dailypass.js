3e6c9649178c7d952d771ee12e5666717dd339e1
bartph <50647914+bartph@users.noreply.github.com>
2022-10-08 11:49:31 +0800

    Update dailypass.js

--- YLGY/dailypass.js
+++ YLGY/dailypass.js
@@ -1,31 +1,30 @@
-// ^https?:\/\/cat-match.easygame2021.com/sheep/v1/game/.*(matchType=3|game_start\?|skin\/info\?)
+// ^https?:\/\/cat-match.easygame2021.com/sheep\/v1\/game\/.*(matchType=3|game_start\?|skin\/info\?)
 // 自动获取所有皮肤,调低第二关难度
 let SkinUrl = "https://cat-match.easygame2021.com/sheep/v1/game/skin/info?";
 let TopUrl = "https://cat-match.easygame2021.com/sheep/v1/game/topic/game_start?";
 let DailyUrl = "https://cat-match.easygame2021.com/sheep/v1/game/map_info_ex?matchType=3";
 let url = $request.url;
+let status = $response.status;
 let body = $response.body;
-switch (url)
+if (url == SkinUrl)
 {
-    case SkinUrl:Skinaction(body);
-    break;
-    case TopUrl:gameAction(body);
-    break;
-    case DailyUrl:gameAction(body);
-    break;
-    default:
-    $done({});
+	$notification.post("脚本进入if语句", "皮肤脚本启动", "皮肤脚本启动");
+	skinAction(body);
+}
+else
+{
+	$notification.post("脚本进入if语句", "游戏脚本启动", "游戏脚本启动");
+	gameAction(body);
 }
 function gameAction(body) {
-	let status = $response.status;
 	if(status == 200){
  	let obj = JSON.parse(body);
 	obj.data.map_seed = [0,0,0,0];
+	$notification.post("游戏难度调整完", "游戏难度调整完", "游戏难度调整完");
 	$done({body: JSON.stringify(obj)});
 }  else $done({});
 }
-function Skinaction(body) {
-	let status = $response.status;
+function skinAction(body) {
 	if(status == 200){
  	let obj = JSON.parse(body);
 	obj.data.skin_list = [];
@@ -33,6 +32,7 @@ function Skinaction(body) {
 { 
 	obj.data.skin_list.push({"id":i});
 }
+	$notification.post("皮肤脚本执行完", "皮肤修改完成", "皮肤修改完成");
 	$done({body: JSON.stringify(obj)});
 }  else $done({});
 }
