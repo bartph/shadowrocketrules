ca56b038c5bcb4522afa0f852e874151c9dfb1fe
bartph <50647914+bartph@users.noreply.github.com>
2022-11-05 19:05:43 +0800

    Update dailypass.js

--- YLGY/dailypass.js
+++ YLGY/dailypass.js
@@ -1,31 +1,43 @@
-var SkinUrl = "https://cat-match.easygame2021.com/sheep/v1/game/skin/info?";
-var TopUrl = "https://cat-match.jianyou2021.com/sheep/v1/game/topic/game_start?";
-var DailyUrl = "https://cat-match.jianyou2021.com/sheep/v1/game/map_info_ex?matchType=3";
+// ^https?:\/\/cat-match.[a-z0-9]+?.com\/sheep\/v1\/game\/.+
+// var SkinUrl,TopUrl,DailyUrl;
+// SkinUrl = 'https://cat-match.easygame2021.com/sheep/v1/game/skin/info?';
+// TopUrl = 'https://cat-match.easygame2021.com/sheep/v1/game/topic/game_start?req_id=1';
+// DailyUrl = 'https://cat-match.easygame2021.com/sheep/v1/game/map_info_ex?matchType=3&req_id=1';
+// $notification.post('撸羊开始执行', '', '分析游戏数据…');
+// switch (url)
+// {
+//     case SkinUrl: skinAction(body);
+//     break;
+//     case TopUrl: gameAction(body);
+//     break;
+//     case DailyUrl: gameAction(body);
+//     break;
+//     default: $done({});
+// }
 var url = $request.url;
 var body = $response.body;
 var obj = JSON.parse(body);
-//$notification.post('撸羊开始执行', '', '分析游戏数据…');
-switch (url)
-{
-    case SkinUrl: skinAction(body);
-    break;
-    case TopUrl: gameAction(body);
-    break;
-    case DailyUrl: gameAction(body);
-    break;
-    default: $done({});
+var regSkin = /skin\/info/i;
+var regTop= /topic\/game_start/i;
+var regDaily = /map_info_ex\?matchType=3/i;
+if(regSkin.test(url)){
+	skinAction(body);
+} else if(regTop.test(url) || regDaily.test(url)){
+	gameAction(body);
+} else {
+	$done({});
 }
 function gameAction(body) {
     $notification.post('截获羊游戏关卡', '', '大幅降低第二幅图难度');
-	obj.data.map_seed = [0,0,0,0];
+	obj['data']['map_seed'] = [0,0,0,0];
 	$done({body: JSON.stringify(obj)});
 }
 function skinAction(body) {
     $notification.post('获取所有皮肤', '', '所有皮肤获取完成');
-	obj.data.skin_list = [];
-	for (var i=0;i<61;i++)
+	obj['data']['skin_list'] = [];
+	for (let i = 0; i < 251; i++)
 { 
-	obj.data.skin_list.push({"id":i});
+	obj['data']['skin_list'].push({"id":i});
 }
 	$done({body: JSON.stringify(obj)});
 }
