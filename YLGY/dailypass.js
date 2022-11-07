3903cfe0b8584a80e3015f8040a7c05ce70911b1
bartph <50647914+bartph@users.noreply.github.com>
2022-11-07 14:19:09 +0800

    Update dailypass.js

--- YLGY/dailypass.js
+++ YLGY/dailypass.js
@@ -28,17 +28,16 @@ if(regSkin.test(url)){
 	$done({});
 }
 function gameAction(body) {
-    $notification.post('截获羊游戏关卡', '', '大幅降低第二幅图难度');
 	obj['data']['map_seed'] = [0,0,0,0];
+	$notification.post('截获羊游戏关卡', '', '大幅降低第二幅图难度');
 	$done({body: JSON.stringify(obj)});
 }
 function skinAction(body) {
 	obj['data']['skin_list'] = [];
 	for (var i = 0; i < 200; i++)
 { 
-	obj['data']['skin_list'].push({"id":i});
+	obj['data']['skin_list'].push({"id":i,"created_at":0,"reason":""});
 }
-
         $notification.post('获取所有皮肤', '', '所有皮肤获取完成');
 	$done({body: JSON.stringify(obj)});
 }
